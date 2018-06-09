dbf80580b755f34476299343df076b2a32b3d5d4
SalmaAhmed95 <salma19956@gmail.com>
2018-06-09 23:27:15 +0200

    home page binding

--- src/main/java/com/tbdbookstore/core/control/HomePageViewController.java
+++ src/main/java/com/tbdbookstore/core/control/HomePageViewController.java
@@ -4,30 +4,49 @@ import com.tbdbookstore.core.Main;
 import com.tbdbookstore.core.pojo.User;
 import com.tbdbookstore.core.uicontrols.shared.LogInDialogControl;
 import com.tbdbookstore.core.uicontrols.shared.SignUpDialogControl;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomePageViewController implements Initializable {
 
     private SignUpDialogControl signUpDialogControl;
-    private LogInDialogControl signInDialogControl;
+    private LogInDialogControl logInDialogControl;
+
+    @FXML
+    StackPane HomePane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         signUpDialogControl = new SignUpDialogControl();
-        signInDialogControl = new LogInDialogControl();
+        logInDialogControl = new LogInDialogControl();
         signUpDialogControl.setOnAcceptClick(e -> {
             if (signUpDialogControl.hasErrors())
                 return;
             User newUser = signUpDialogControl.getValue();
-            System.out.println(newUser);
-            //TODO switch to MANAGER or USER VIEW
+            //TODO send new user object to jdbc
             signUpDialogControl.close();
+            //switch to user view
+            switchView("/com/tbdbookstore/view/fxml/user/User.fxml");
+
         });
-      //TODO ADD SAME ACCEPT ACTION to login
+        logInDialogControl.setOnAcceptClick(e -> {
+            if (logInDialogControl.hasErrors())
+                return;
+            String userName = logInDialogControl.getUserName();
+            String password = logInDialogControl.getPassword();
+            //TODO send log in data to jdbc and check role to load certain view
+
+
+        });
     }
 
     public void signUp(MouseEvent mouseEvent) {
@@ -35,6 +54,16 @@ public class HomePageViewController implements Initializable {
     }
 
     public void LogIn(MouseEvent mouseEvent) {
-        signInDialogControl.show(Main.getRoot());
+        logInDialogControl.show(Main.getRoot());
     }
+
+    public void switchView(String path) {
+        try {
+            Pane pane = FXMLLoader.load(getClass().getResource(path));
+            HomePane.getChildren().setAll(pane);
+        } catch (IOException ex) {
+
+        }
+    }
+
 }
